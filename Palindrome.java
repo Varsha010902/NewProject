5a386534d4e2f83bb0de0e47c985de765a527d18
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-08-01 21:02:11 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,13 +1,5 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
 import java.util.Scanner;
-public class Main{
+public class Palindrome{
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         String str=sc.nextLine();
