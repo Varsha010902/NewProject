99f1894e9f2b0fe65bd3d8fe6dc007838067eb4b
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-08-07 19:25:07 +1300

    Add files via upload

---
+++ ReverseString.java.java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+import java.util.*;
+class Main
+{
+    public static void main(String[]args){
+        String str="Varsha Swathi";
+        String revStr="";
+        for(int i=str.length()-1;i>=0;i--){
+            revStr=revStr+str.charAt(i);
+        }
+        System.out.println("OriginalString:"+str);
+        System.out.println("ReversedString:"+revStr);
+    } 
+}
+    
