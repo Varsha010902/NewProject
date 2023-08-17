d22d8e7a6d6212f982c650ed64454d3d185a263b
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-08-17 14:59:17 +0530

    Update Binary_Search.java

--- Binary_Search.java
+++ Binary_Search.java
@@ -1,12 +1,5 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 import java.util.*;
-public class Main
+public class BinarySearch
 {
 	public static void main(String[] args) {
 		int a[]={1,2,3,4,5};
