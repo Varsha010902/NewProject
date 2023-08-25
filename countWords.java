17a463b25fc07b63fdd69a34b33a40003070f870
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-08-25 18:46:54 +0530

    Update countWords.java

--- countWords.java
+++ countWords.java
@@ -6,7 +6,7 @@ Write your code in this editor and press "Run" button to execute it.
 
 *******************************************************************************/
 import java.util.Scanner;
-public class Main
+public class countWords
 {
 	public static void main(String[] args) {
 	    
