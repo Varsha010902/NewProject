766667d97bf0a878ed3cf4dcff66ffaa17a1a521
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-08-28 20:22:03 +0530

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
-public class Main {
+public class Anagram{
   public static void main(String[] args) {
       Scanner sc=new Scanner(System.in);
       System.out.println("Enter a str1:");
