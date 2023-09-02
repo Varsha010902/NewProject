8b3839466f9dfa4ccdd273360e97ba677ffe597e
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-09-02 21:33:15 +0530

    Add files via upload

---
+++ spliting_word.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+
+
+    {
+        String str = "Geeks.for.Geeks";
+        String[] str1 = str.split("[.]"); 
+        for (String i : str1)
+            System.out.println(i);
+    }
+}
+		
+		
+	}
+
