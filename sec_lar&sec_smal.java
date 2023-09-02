ea600ffbc2d7ae3cb1b7164b9c313b10deccc09d
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-09-02 22:07:02 +0530

    Add files via upload

---
+++ sec_lar&sec_smal.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+public class Main
+{
+	public static void main(String[] args) {
+	    int len=6;
+	    int[] arr={3,2,1,7,5,4};
+	    if(len==0||len<3){
+	        System.out.print(0);
+	    }
+	    int eve_arr[]=new int[len/2];
+	    int odd_arr[]=new int[len/2];
+	 /declaring index/   int e=0;
+	 /*declaring index */  int o=0;
+	 for(int i=0;i<len;i++){
+	     if(i%2==0){
+	         eve_arr[e++]=arr[i];
+	         
+	     }else{
+	         odd_arr[o++]=arr[i];
+	     }
+	     
+	 }
+	 Arrays.sort(eve_arr);
+	 Arrays.sort(odd_arr);
+	 int sec_lar=eve_arr[eve_arr.length-2];
+	 int sec_sma=odd_arr[1];
+	 System.out.println(sec_lar + sec_sma);
+	    
+}
+
+}
+
+
