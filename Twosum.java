9239059638d75f46b1a5493132406e7eccc1f412
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-09-23 15:03:27 +0530

    Update Twosum.java

--- Twosum.java
+++ Twosum.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Main {
+public class TwoSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter n:");
