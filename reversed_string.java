97631f5b8f7ee2f058f12b4a792da3f738c066af
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-10-02 15:23:09 +0530

    Update reversed_string.java

--- reversed_string.java
+++ reversed_string.java
@@ -1,19 +1,15 @@
 import java.util.*;
-public class Main{
+public class reverseString{
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         String s =sc.nextLine();
         String reverse="";
         String[] s1=s.split("\\.");
-        for(int i=s1.length-1;i>=0;i--){
-            
-            reverse+=s1[i];
-            
+        for(int i=s1.length-1;i>=0;i--){            
+            reverse+=s1[i];            
         if(i>0){
            reverse+=".";
-        }
-        
-        
+        }        
     } System.out.println(reverse);
         
     }
