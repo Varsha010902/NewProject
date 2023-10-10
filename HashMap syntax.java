c4fb9e8e5ad2be9dce72d5d82dfccb6d3aff820d
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-10-10 22:32:35 +0530

    Add files via upload

---
+++ HashMap syntax.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+public class Main
+{
+	public static void main(String[] args) {
+		HashMap<Integer,String> map=new HashMap<>();
+		map.put(1,"ant");
+		map.put(2,"book");
+		
+		System.out.print("Mapping:"+map);
+	}
+}
