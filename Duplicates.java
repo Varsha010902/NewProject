efa803ec2678c8393bb515a60d6e991e3465f90b
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-11-08 21:41:49 +0530

    Update Duplicates.java

--- Duplicates.java
+++ Duplicates.java
@@ -1,13 +1,6 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 import java.util.*;
 
-public class Main {
+public class Duplicates {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
