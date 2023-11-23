22e7ea740fcfca28f4e5251d04d466ca4a9e15c1
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2023-11-23 12:49:36 +0530

    Add files via upload

---
+++ AsciiValue.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AsciiValue{
+    public static void main(String[] args){
+      
+        char ch='a';
+        int res=(int) ch; //finding ascii value of a character
+        System.out.print(res);
+
+    }
+}
