1f41d22451990f190502e6a41d1b6ece9bdddbea
Varsha010902 <139903303+Varsha010902@users.noreply.github.com>
2024-02-20 18:49:12 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -11,4 +11,16 @@ class ReverseString
         System.out.println("ReversedString:"+revStr);
     } 
 }
+/////////////////////////////////////////////////////////////
+#using stringmanipulation
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    String str="varsha";
+	    StringBuilder rev=new StringBuilder(str).reverse();
+	    System.out.print(rev);		
+	}
+}
     
